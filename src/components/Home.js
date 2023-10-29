4f95525d326891704a6fccd5c6d7f1c540c6b0c9
Sayala100 <s.ayalac@uniandes.edu.co>
2023-10-29 18:23:24 -0500

    go

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import * as filestack from 'filestack-js';
 import React, { useState } from 'react';
 import axios from 'axios'; // Import the axios library
+import '../styles/Home.css';
 
 function Home() {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -12,21 +15,21 @@ function Home() {
 
   const handleUpload = () => {
     if (selectedFile) {
-      console.log('Uploading file:', selectedFile);
+      setUploading(true);
       const client = filestack.init("ASOcJeKFZS1m5rYyTVyFFz");
       client.upload(selectedFile).then(data => {
         const urlFile = data.url;
-        console.log('Uploaded:', urlFile);
-        axios.post(
-          'http://localhost:8000/files/',
-          { 
-            url: urlFile
-          }
-          )
+        axios.post('http://localhost:8000/files/', { url: urlFile })
           .then(response => {
+            setUploading(false);
+            setSuccess(true);
+            setTimeout(() => {
+              setSuccess(false);
+            }, 3000);
             console.log('Processed file link is:', response.data);
           })
           .catch(error => {
+            setUploading(false);
             console.error('Error sending file link to the backend:', error);
           });
       });
@@ -36,11 +39,14 @@ function Home() {
   };
 
   return (
-    <div style={{ "marginInlineStart": "200px" }}>
-      <h1>Bienvenido, aquí puedes subir un archivo para categorizar los textos!</h1>
+    <div className="container">
+      <h1 style={{ backgroundColor: '#f9f9f9', padding: '10px' }}>Bienvenido, aquí puedes subir un archivo para categorizar los textos!</h1>
+      
       <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
       {selectedFile && <p>Selected File: {selectedFile.name}</p>}
+      <hr />
+      <button onClick={handleUpload}>{uploading ? 'Cargando...' : 'Upload'}</button>
+      {success && <div className="success-popup">¡Carga exitosa!</div>}
     </div>
   );
 }
