b7ca7df57753ec42cd4ba388ba31d5a8d871a18b
leo283 <germanclarinet03@gmail.com>
2023-10-29 12:41:18 -0500

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+import * as filestack from 'filestack-js';
+import React, { useState } from 'react';
+import axios from 'axios'; // Import the axios library
+
+function Home() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleUpload = () => {
+    if (selectedFile) {
+      console.log('Uploading file:', selectedFile);
+      const client = filestack.init("ASOcJeKFZS1m5rYyTVyFFz");
+      client.upload(selectedFile).then(data => {
+        const urlFile = data.url;
+        axios.post('http://localhost:8000/upload', { fileLink: urlFile })
+          .then(response => {
+            console.log('File link sent to the backend:', response.data);
+          })
+          .catch(error => {
+            console.error('Error sending file link to the backend:', error);
+          });
+      });
+    } else {
+      console.log('No file selected.');
+    }
+  };
+
+  return (
+    <div style={{ "marginInlineStart": "200px" }}>
+      <h1>Bienvenido, aquí puedes subir un archivo para categorizar los textos!</h1>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+      {selectedFile && <p>Selected File: {selectedFile.name}</p>}
+    </div>
+  );
+}
+
+export default Home;
